40e0a585e8e7a36bb2e4bf96fe1f0555de3c157d
yulya <bodrova.ium@phystech.edu>
2021-05-14 22:38:43 +0300

    Добавлены методы

--- HashTable.h
+++ HashTable.h
@@ -1,18 +1,44 @@
-struct Node{
+using namespace std;
+
+class Node{
+private:
 	string value;
 	Node * next;
+public:
+	Node(){};
+	Node( const string & key ){};
+	string GetValue() const {};
+	void SetNext(Node * ptr) {};
+	Node *GetNext() const {};
 };
 
-struct LinkedList{
+class LinkedList{
+public:
 	LinkedList(){};
 	~LinkedList(){};
 	bool Find(const string & key){};
 	void Insert(const string & key){};
 	void Delete(const string & key){};
+
+private:
+	Node * last;
+	Node * head;
+	ErrorType error;
 };
 
-struct HashTable{
+class HashTable{
+public:
+HashTable(const int &, int (*)(const string &)){};
+~HashTable(){};	
+void Insert(const string & ){};
+bool Find(const string &) const{};
+void Delete(const string & ){};
+int Size() const{};
+LinkedList* Table() const{};
+
+private:
 	int (*HashFunction)(const string &);
+	ErrorType error;
 	LinkedList* table;
 	int size = 0;
 };
