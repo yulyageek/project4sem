ea19113b9d0c258f57e10e40d14eb2059cd4fa51
yulya <bodrova.ium@phystech.edu>
2021-05-15 00:00:53 +0300

    проверка

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <sstream>
+#include "HashTable.h"
+
+using namespace std;
+
+int MyHash(const string & x){
+	int len = x.size();
+	int result = 0;
+	for (int i = 0; i < len; i++){
+		result += (int)(x[i]);
+	}
+	return result;
+}
+
+int main(){
+	HashTable T(5, MyHash);
+	return 0;
+}
