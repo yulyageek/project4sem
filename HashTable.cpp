cf9bccee4ef81262eaa73e597f2930227ecb00e1
yulya <bodrova.ium@phystech.edu>
2021-05-14 23:54:49 +0300

    +методы класса Node

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,1 +1,18 @@
 #include "HashTable.h"
+
+Node::Node(){
+	next = NULL;
+}
+Node::Node( const string & key ){
+	value = key;
+	next = NULL;
+}
+string Node::GetValue() const {
+	return value;
+}
+void Node::SetNext(Node * ptr) {
+	next = ptr;
+}
+Node * Node::GetNext() const {
+	return next;
+}
