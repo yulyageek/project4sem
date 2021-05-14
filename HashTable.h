c7681a6000089da2c3e0689c7e11857070090f47
yulya <bodrova.ium@phystech.edu>
2021-05-14 22:37:35 +0300

    Определены структура таблицы и некоторые вспомогательные структры

--- HashTable.h
+++ HashTable.h
@@ -1,1 +1,19 @@
-.
+struct Node{
+	string value;
+	Node * next;
+};
+
+struct LinkedList{
+	LinkedList(){};
+	~LinkedList(){};
+	bool Find(const string & key){};
+	void Insert(const string & key){};
+	void Delete(const string & key){};
+};
+
+struct HashTable{
+	int (*HashFunction)(const string &);
+	LinkedList* table;
+	int size = 0;
+};
+
