b568421e0b45d1efc86018391bc7b151f2e84fea
yulya <bodrova.ium@phystech.edu>
2021-05-19 10:32:33 +0300

    тест работы шаблонов

--- test.cpp
+++ test.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <sstream>
-#include <exception>
-#include "HashTable.h"
+#include "HashTable.cpp"
 
 using namespace std;
 
-int MyHash(const string & x){
+int MyHashString(const string & x){
 	int len = x.size();
 	int result = 0;
 	for (int i = 0; i < len; i++){
@@ -14,33 +13,38 @@ int MyHash(const string & x){
 	return result;
 }
 
+int MyHashInt(const int & x){
+	return (x*x)%73;
+}
+
+
 int main(){
 	try{
-		HashTable T(-5, MyHash);
+		HashTable<string> T(5, MyHashString);
+		T.Insert("word");
+		T.Insert("namespace");
+		T.Insert("include");
+		T.Insert("hash");
+		cout << T;
+		T.Delete("include");
+		T.Insert("result");
+		T.Delete("result");
+		if (T.Size() == 5){
+			cout << "size test done" << endl;
+		}
+		if (T.Find("word") == 1 and T.Find("table") == 0){
+			cout << "find test done" << endl;
+		}
 	} catch (ErrorType error){
 		Error(error); 
 	}
 
 	try{
-		HashTable T(5, MyHash);
+		HashTable<int> T(-4, MyHashInt);
+		T.Insert(125);
+		T.Insert(14);
 	} catch (ErrorType error){
 		Error(error); 
 	}
-	HashTable T(5, MyHash);
-	T.Insert("word");
-	T.Insert("namespace");
-	T.Insert("include");
-	T.Insert("hash");
-	cout << T;
-	T.Delete("include");
-	T.Insert("result");
-	T.Delete("result");
-	if (T.Size() == 5){
-		cout << "size test done" << endl;
-	}
-	if (T.Find("word") == 1 and T.Find("table") == 0){
-		cout << "find test done" << endl;
-	}
-	cout << T;
 	return 0;
 }
