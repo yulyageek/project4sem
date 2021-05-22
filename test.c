0a388ab39ca6750aab884515ec9d9640231bb25d
yulya <bodrova.ium@phystech.edu>
2021-05-22 03:55:19 +0300

    новые тесты, покрытие 96%

--- test.c
+++ test.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "HashTable.h"
-#include <sys/types.h>
-#include <signal.h>
-#include <unistd.h>
-
 
 int main(){
 	HT_ERR err;
+	Table * p;
+	p = init_Hash_Table(-5, &err);
+	if (err == INVARG) printf("test done\n");
  	Table * t;
 	t = init_Hash_Table(5, &err);
+	add_new("test", NULL, &err);
+	add_new("botay", t, &err);
+	add_new("yulya", t, &err);
+	add_new("test", t, &err);
+	add_new("error", t, &err);
 	add_new("aaaa", t, &err);
 	add_new("aabb", t, &err);
-	//p = init_Hash_Table(3, &err);
-	add_new("pizda", t, &err);
 	print_Table(t, &err);
+	Search("what ever", NULL, &err);
+	Search("test", NULL, &err);
+	Delete("test", NULL, &err);
+	Delete("test", t, &err);
+	Delete("aaaa", t, &err);
+	Delete("botay", t, &err);
+	Delete("yulya", t, &err);
+	Delete("trash", t, &err);
+	print_Table(NULL, &err);
+	if (err == INVARG) printf("test done\n");
 	remove_Table(t, &err);
+	remove_Table(NULL, &err);
+	if (err == INVARG) printf("test done\n");
 }
 
